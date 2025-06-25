5e63dc41db965e6e3a6b0fe2c3d300a63f014b65
arnursultan <badasspubgm@gmail.com>
2025-06-26 01:18:19 +0600

    profiles serializers up

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,7 +5,7 @@ from .models import StudentProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'role', 'created_at')
+        fields = ('id', 'email', 'full_name', 'role', 'created_at')
         read_only_fields = ('id', 'role', 'created_at', 'email')
 
 
